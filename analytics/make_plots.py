c5a4f9a99cf9f69c033e22630f8788b38074236e
Joe Wong <>
2021-09-21 21:03:20 -0700

    create more queries; create more plots

---
+++ analytics/make_plots.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import seaborn as sns
+import datetime as datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+from pathlib import Path
+
+
+def convert_day(x):
+    if x == 0: x = "Sunday"
+    elif x == 1: x = "Monday"
+    elif x == 2: x = "Tuesday"
+    elif x == 3: x ="Wednesday"
+    elif x == 4: x = "Thursday"
+    elif x == 5: x = "Friday"
+    elif x == 6: x = "Saturday"
+    return x
+
+def group_date_times():
+    """ad-hoc function to read the data and format it for plots
+    that plot the number of games by time"""
+    df = pd.read_csv("./query_out_storage/games_played_by_time_of_day.csv")
+    #format hours and minutes into date_time
+    df["time"] = df.apply(lambda x: datetime.time(int(x["hour"]), int(x["minute"])), axis=1)
+    df["time_str"] = df["time"].apply(str)
+    df["time_int"] = df.apply(lambda x: x["hour"]*60 + x["minute"], axis=1)
+
+    #grouping into 48 "bins", one for each 30 minutes in a day
+    df["time_group"] = pd.qcut(df["time_int"],48)
+    df["day"] = df["day"].apply(convert_day)
+    return df
+
+def barplot_games_by_time_of_day(filename):
+    df = group_date_times()
+    df2 = df.groupby(by="time_group").sum()
+    #ordering color palette by number_of_games
+    pal = sns.color_palette("flare_r", df2.shape[0])
+    rank = df2["number_of_games"].argsort().argsort()
+    #creating barplot
+    bar = sns.barplot(x=df2.index, y="number_of_games", data=df2, palette=np.array(pal)[rank])
+    bar.set(xlabel='UTC Time', ylabel='Total Games Played (Millions)')
+    bar.set_xticks(range(0,48,2))
+    bar.set_xticklabels(range(0,24))
+    bar.set_title("Total Number of Games Played by Time of Day")
+    fig = bar.get_figure()
+    fig.savefig(filename, dpi=300)
+
+def barplot_games_by_day(filename):
+    df = group_date_times()
+    df2 = df.groupby(by="day").sum()
+    df2 = df2.sort_values(by="number_of_games")
+    bar = sns.barplot(x=df2.index, y="number_of_games", data=df2, palette="flare_r")
+    bar.set(xlabel='Day of the Week', ylabel='Total Games Played (Millions)')
+    bar.set_title("Total Number of Games Played by Day of the Week")
+    fig = bar.get_figure()
+    fig.savefig(filename, dpi=300)
+
+def lineplot_games_by_time_and_day(filename):
+    df = group_date_times()
+    p = sns.relplot(data=df, x="time_int", y="number_of_games", hue="day", kind="line", linewidth=0.3)
+    for ax in p.axes.flat:
+        labels = ax.get_xticklabels()
+        ax.set_xticks(ticks=range(0,24*60,60))
+        ax.set_xticklabels(fontsize=8, labels=range(0,24))
+        ax.set_title("Total Number of Games Played by Time and Day")
+    p.set(xlabel='UTC Time', ylabel='Total Games Played')
+    p.savefig(filename, dpi=300)
+
+def pieplot_games_per_elo_band(filename):
+    df = pd.read_csv("query_out_storage/total_games_per_elo_bracket.csv")
+    df = df.sort_values(by="elo_bracket")
+    #reordering > 600 and > 800 elo brackets
+    df = pd.concat([df.iloc[[11],:], df.drop(11, axis=0)], axis=0)
+    df = pd.concat([df.iloc[[11],:], df.drop(10, axis=0)], axis=0)
+    #creating pie plot
+    pct = 100*df.total_games/df.total_games.sum()
+    labels = ["{0}: {1:1.2f} %".format(i,j) for i,j in zip(df.elo_bracket, pct)]
+    pie, ax = plt.subplots(figsize=[10,6])
+    patches, text = plt.pie(x=df.total_games, wedgeprops={'linewidth':1, 'linestyle':'-', 'edgecolor':'k'}, \
+            pctdistance=1, startangle=90)
+    ax.set_title("Total Number of Games Played per Elo Bracket")
+    plt.legend(patches, labels, loc='center right', bbox_to_anchor=(1.4, .5),
+           fontsize=8)
+    plt.savefig(filename, dpi=300, bbox_inches='tight')
+
+def histogram_player_churn(filename):
+    df = pd.read_csv("query_out_storage/total_games_per_player.csv")
+    h, ax = plt.subplots(figsize=[10,6])
+    n, bins, patches = plt.hist(df.total_games, bins=200, log=True, histtype='step')
+    ax.set_title("Total Games Played per Player Histogram")
+    ax.set_xlabel("Number of Games Played")
+    ax.set_ylabel("Number of Players")
+    plt.xticks(np.arange(0, max(df.total_games)+1, 10000))
+    plt.xlim([0,100000])
+    plt.savefig(filename, dpi=300, bbox_inches='tight')
+    print("median games played per player: ", np.median(df.total_games))
+    print("average games played per player: ", np.average(df.total_games))
+    print("maximum games played by one player: ", max(df.total_games))
+
+def barplot_pct_analyzed_per_elo_bracket(filename):
+    df = pd.read_csv("query_out_storage/pct_analyzed_per_elo_bracket.csv")
+    df["pct_analyzed"] = df["analyzed_games"]/df["total_games"]*100
+    #reordering > 600 and > 800 elo brackets
+    df = pd.concat([df.iloc[[11],:], df.drop(11, axis=0)], axis=0)
+    df = pd.concat([df.iloc[[11],:], df.drop(10, axis=0)], axis=0)
+    #ordering color palette by pct_analyzed
+    pal = sns.color_palette("flare_r", df.shape[0])
+    rank = df["pct_analyzed"].argsort().argsort()
+    #creating barplot
+    bar = sns.barplot(x="elo_bracket", y="pct_analyzed", data=df, palette=np.array(pal)[rank])
+    bar.set(xlabel='elo_bracket', ylabel='pct_analyzed (%)')
+    bar.set_title("Percentage of Games Analyzed by Elo Bracket")
+    plt.setp(bar.xaxis.get_majorticklabels(), rotation=45, ha="right")
+    fig = bar.get_figure()
+    fig.savefig(filename, dpi=300, bbox_inches='tight')
+
+def lineplot_elo_over_time(filename):
+    print("reading csv...")
+    df = pd.read_csv("query_out_storage/total_blitz_games_per_player_over_time.csv")
+    print("converting days since start...")
+    df["days_since_start"] = df["days_since_start"].apply(lambda x: int(re.search(r'\d+', x).group(0)))
+    df_starting_elo = df[df["days_since_start"] == 0]
+    for lower_elo_lim in range(800,2201,200):
+        upper_elo_lim = lower_elo_lim + 199
+        df_band = df_starting_elo[(df_starting_elo["min"] >= lower_elo_lim) & (df_starting_elo["min"] <= upper_elo_lim)]
+        df.loc[df["player"].isin(df_band["player"]), "band"] = f"{lower_elo_lim} - {upper_elo_lim}"
+    df = df.dropna()    #drop extreme elo values (600-800 and 2400+) due to low sample size
+    #print("plotting...")
+    ax = sns.lineplot(x="days_since_start", y="min", hue="band", data=df)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0,box.width*.8, box.height])
+    handles, labels = ax.get_legend_handles_labels()
+    labels, handles = zip(*sorted(zip(labels, handles), key=lambda x: int(x[0][0:4])))  #sort by elo number instead of str
+    ax.legend(handles, labels, loc='center left', bbox_to_anchor=(1,.5), title="starting elo")
+    ax.set_xlabel("days since player's first stable rating")
+    ax.set_ylabel('stable elo rating')
+    ax.set_title('elo rating over time (95% confidence interval)')
+    fig = ax.get_figure()
+    print("saving figure...")
+    fig.savefig(filename, dpi=300)
+
+if __name__ == "__main__":
+    Path("./plots/popular_play_times").mkdir(parents=True, exist_ok=True)
+    #barplot_games_by_time_of_day("./plots/popular_play_times/games_by_time_of_day.png")
+    #lineplot_games_by_time_and_day("./plots/popular_play_times/games_by_time_and_day.png")
+    #barplot_games_by_day("./plots/popular_play_times/games_by_day.png")
+    #pieplot_games_per_elo_band("./plots/games_per_elo_bracket.png")
+    #histogram_player_churn("./plots/games_per_player.png")
+    #barplot_pct_analyzed_per_elo_bracket("./plots/pct_analyzed_per_elo_bracket.png")
+    #lineplot_elo_over_time("./plots/blitz_games_per_player_over_time.png")
--- analytics/psycopg2_query.py
+++ analytics/psycopg2_query.py
@@ -34,7 +34,9 @@ if __name__ == "__main__":
 
     queries = []
     filenames = []
-    q0 = "EXPLAIN SELECT * FROM Games;"
+    #baseline query ~30 minutes to run, 7.5M disk page fetches
+    q0 = "EXPLAIN SELECT COUNT(*) FROM Games;"
+
     q1 = """
     (SELECT * FROM GAMES g JOIN user_ids u ON g.white = u.id
     WHERE g.whiteratingdiff < 30
@@ -45,6 +47,7 @@ if __name__ == "__main__":
     limit 100);
     """
     q2 = """
+    EXPLAIN
     SELECT EXTRACT(DOW FROM date_time) as Day, 
     EXTRACT(HOUR FROM date_time) as Hour, 
     EXTRACT(MINUTE FROM date_time) as Minute,
@@ -61,8 +64,119 @@ if __name__ == "__main__":
     FROM GAMES WHERE white = 101000 OR black = 101000
     ORDER BY event, date_time
     """
+    q4 = """
+    SELECT sub1.white, sub1.white_game_count + sub2.black_game_count as total_games
+    FROM
+    (SELECT White, COUNT(*) as white_game_count
+    FROM games g1
+    GROUP BY White) as sub1
+    JOIN
+    (SELECT Black, COUNT(*) as black_game_count
+    FROM games g2
+    GROUP BY Black) as sub2
+    ON sub1.white = sub2.black
+    ORDER BY total_games DESC;
+    """
+    q5 = """
+    SELECT
+    CASE WHEN LEAST(Whiteelo, Blackelo) < 600 THEN 'elo < 600'
+    WHEN LEAST(Whiteelo, Blackelo) >= 600 AND LEAST(Whiteelo, Blackelo) < 800 THEN '600 <= elo < 800'
+    WHEN LEAST(Whiteelo, Blackelo) >= 800 AND LEAST(Whiteelo, Blackelo) < 1000 Then '800 <= elo < 1000'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1000 AND LEAST(Whiteelo, Blackelo) < 1200 Then '1000 <= elo < 1200'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1200 AND LEAST(Whiteelo, Blackelo) < 1400 Then '1200 <= elo < 1400'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1400 AND LEAST(Whiteelo, Blackelo) < 1600 Then '1400 <= elo < 1600'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1600 AND LEAST(Whiteelo, Blackelo) < 1800 Then '1600 <= elo < 1800'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1800 AND LEAST(Whiteelo, Blackelo) < 2000 Then '1800 <= elo < 2000'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2000 AND LEAST(Whiteelo, Blackelo) < 2200 Then '2000 <= elo < 2200'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2200 AND LEAST(Whiteelo, Blackelo) < 2400 Then '2200 <= elo < 2400'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2400 AND LEAST(Whiteelo, Blackelo) < 2600 Then '2400 <= elo < 2600'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2600 AND LEAST(Whiteelo, Blackelo) < 2800 Then '2600 <= elo < 2800'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2800 Then '2800 <= elo' END as elo_bracket,
+    COUNT(*) as total_games
+    FROM games
+    GROUP by 1;
+    """
 
-    queries = [q2]
-    select_query(queries, conn)
+    q6 = """
+    SELECT sub1.white, sub1.event, sub1.white_game_count + sub2.black_game_count as total_games
+    FROM
+    (SELECT White, event, COUNT(*) as white_game_count
+    FROM games g1
+    GROUP BY White, event) as sub1
+    JOIN
+    (SELECT Black, event, COUNT(*) as black_game_count
+    FROM games g2
+    GROUP BY Black, event) as sub2
+    ON sub1.white = sub2.black
+    AND sub1.event = sub2.event
+    ORDER BY total_games DESC;
+    """
+
+    q7 = """
+    SELECT event, count(*) 
+    FROM games
+    GROUP BY event;
+    """
+
+    q8 = """
+    EXPLAIN
+    SELECT
+    CASE WHEN LEAST(Whiteelo, Blackelo) < 600 THEN 'elo < 600'
+    WHEN LEAST(Whiteelo, Blackelo) >= 600 AND LEAST(Whiteelo, Blackelo) < 800 THEN '600 <= elo < 800'
+    WHEN LEAST(Whiteelo, Blackelo) >= 800 AND LEAST(Whiteelo, Blackelo) < 1000 Then '800 <= elo < 1000'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1000 AND LEAST(Whiteelo, Blackelo) < 1200 Then '1000 <= elo < 1200'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1200 AND LEAST(Whiteelo, Blackelo) < 1400 Then '1200 <= elo < 1400'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1400 AND LEAST(Whiteelo, Blackelo) < 1600 Then '1400 <= elo < 1600'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1600 AND LEAST(Whiteelo, Blackelo) < 1800 Then '1600 <= elo < 1800'
+    WHEN LEAST(Whiteelo, Blackelo) >= 1800 AND LEAST(Whiteelo, Blackelo) < 2000 Then '1800 <= elo < 2000'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2000 AND LEAST(Whiteelo, Blackelo) < 2200 Then '2000 <= elo < 2200'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2200 AND LEAST(Whiteelo, Blackelo) < 2400 Then '2200 <= elo < 2400'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2400 AND LEAST(Whiteelo, Blackelo) < 2600 Then '2400 <= elo < 2600'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2600 AND LEAST(Whiteelo, Blackelo) < 2800 Then '2600 <= elo < 2800'
+    WHEN LEAST(Whiteelo, Blackelo) >= 2800 Then '2800 <= elo' END as elo_bracket,
+    SUM(CASE WHEN analyzed THEN 1 ELSE 0 END) as analyzed_games,
+    COUNT(*) as total_games
+    FROM games
+    GROUP by 1;
+    """
+
+    q9 = """
+    EXPLAIN
+    SELECT g1.White as player, g1.whiteelo as elo, g1.event, g1.date_time
+    FROM games g1
+    UNION ALL
+    SELECT g1.Black as player, g1.blackelo as elo, g1.event, g1.date_time
+    FROM games g1;
+    """
+    #select min and max ratings per player with start and end dates of activity (black games)
+    q10 = """
+    SELECT player, event, min(elo), max(elo), min(date_time), max(date_time) FROM
+    (SELECT white as player, whiteelo as elo, event, date_time
+    FROM games
+    WHERE ABS(whiteratingdiff) < 30
+    UNION ALL
+    SELECT black as player, blackelo as elo, event, date_time
+    FROM games
+    WHERE ABS(blackratingdiff) < 30) as s
+    GROUP BY player, event;
+    """
+    q11 = """
+    SELECT g.Black as player, MIN(g.blackelo), g.date_time::date, 
+            AGE(g.date_time::date, Min(sub.start_date)) as days_since_start
+    FROM games g
+    JOIN (SELECT g2.black, min(g2.date_time::date) as start_date
+            FROM games g2
+            WHERE g2.event = 'B'
+            AND ABS(g2.blackratingdiff) < 30
+            GROUP BY g2.black) sub
+    on g.black = sub.black
+    WHERE g.event = 'B'
+    AND ABS(g.blackratingdiff) < 30
+    GROUP BY g.Black, g.date_time::date
+    """
+
+    queries = [q11]
+    filenames = ["q11.csv"]
+    select_query(queries, conn, filenames=filenames)
 
     #conn.commit()
